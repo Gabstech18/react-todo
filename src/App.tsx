81408f559af16c6c4cc2b24a015abcebadbe1866
Gabino Hernandez <gabino.hern.g@gmail.com>
2024-08-18 17:51:24 -0600

    Added a separated function to control the counter (useless)

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import Card from "./components/Card";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const handleCounter = () => {
+  setCount(count + 1);
+}
   return (
     <>
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increase counter</button>
+      <button onClick={handleCounter}>Increase counter</button>
 
       <Card />
     </>
