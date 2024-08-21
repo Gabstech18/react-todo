8fa312e33ba93abe2659faabe4c2e38170dcbd0e
Gabino Hernandez <gabino.hern.g@gmail.com>
2024-08-21 10:00:08 -0600

    Added some elements to structure the card 	modified:   src/components/Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,45 @@
-
+//import "./Card.css";
 
 const Card = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <>
+      <div className="card-header">
+        <p>Hello</p>
+      </div>
+      <div className="card-title">
+        <h1>To Do Today</h1>
+      </div>
+
+      <div className="card-quote">
+        <p className="quote-content">
+          Give me six hours to chop down a tree and I will spend the first four
+          sharpening the axe.
+        </p>
+        <p className="quote-author">Abraham Lincoln</p>
+      </div>
+
+      <div className="card-items">
+        {/* Aquí puedes agregar la lógica para mostrar los ítems */}
+      </div>
+
+      <div className="card-item-creator">
+        <input
+          type="text"
+          id="item-input"
+          name="item"
+          placeholder="Add a new item"
+        />
+        <select name="item-priority" id="item-priority">
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+          <option value="D">D</option>
+          <option value="E">E</option>
+        </select>
+        <button>Add</button>
+      </div>
+    </>
   );
 };
+
 export default Card;
